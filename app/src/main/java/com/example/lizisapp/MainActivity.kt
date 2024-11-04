256d2a3006e141fa7bd312b4eb39a8fc9215aece
student <student@BTU.local>
2024-11-04 13:23:59 +0400

    Update project files

--- app/src/main/java/com/example/lizisapp/MainActivity.kt
+++ app/src/main/java/com/example/lizisapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.lizisapp
 
-import androidx.appcompat.app.AppCompatActivity
+import com.example.lizisapp.R
+
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val okButton = findViewById<Button>(R.id.okButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+
+        okButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            if (email.isNotEmpty()) {
+                Toast.makeText(this, "Email saved: $email", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        nextButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            if (email.isNotEmpty()) {
+
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("EMAIL", email)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
+
+
+
+
--- app/src/main/java/com/example/lizisapp/SecondActivity.kt
+++ app/src/main/java/com/example/lizisapp/SecondActivity.kt
@@ -1,4 +1,37 @@
 package com.example.lizisapp
+import com.example.lizisapp.R
 
-class SecondActivity {
+
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeTextView)
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val clickButton = findViewById<Button>(R.id.clickButton)
+
+
+        val email = intent.getStringExtra("EMAIL")
+        welcomeTextView.text = "Welcome back\n$email"
+
+
+        numberTextView.text = counter.toString()
+
+
+        clickButton.setOnClickListener {
+            counter--
+            numberTextView.text = counter.toString()
+        }
+    }
 }
